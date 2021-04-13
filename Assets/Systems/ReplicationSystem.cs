d47a8900f23e8bcfa770f8d3b9c956e7a28d4029
jacksonlli <59549394+jacksonlli@users.noreply.github.com>
2021-04-13 17:08:42 -0400

    Restructure code for better reconciliation

--- Assets/Systems/ReplicationSystem.cs
+++ Assets/Systems/ReplicationSystem.cs
@@ -73,10 +73,11 @@ public class ReplicationSystem : ISystem
     {
         // apply state from server
         // can receive only one replication message per entity for simplicity
-        ComponentsManager.Instance.ForEach<ReplicationMessage>((entityID, msgReplication) => {
-            // Updating entity info from message's state
-            var component = ComponentsManager.Instance.GetComponent<ShapeComponent>(msgReplication.entityId);
 
+        //this loop is for reshaping entities into players
+        ComponentsManager.Instance.ForEach<ReplicationMessage>((entityID, msgReplication) =>
+        {
+            var component = ComponentsManager.Instance.GetComponent<ShapeComponent>(msgReplication.entityId);
             if (component.shape != msgReplication.shape)
             {
                 // needs to respawn entity to change its shape
@@ -89,120 +90,131 @@ public class ReplicationSystem : ISystem
                 spawnInfo.replicatedEntitiesToSpawn.Add(msgReplication);
                 ComponentsManager.Instance.SetComponent<SpawnInfo>(new EntityComponent(0), spawnInfo);
             }
-            else
+        });
+
+        //this loop is for updating the entity client histories to only contain states with time equal or larger than the timeCreated of the server replication message
+        ComponentsManager.Instance.ForEach<ReplicationMessage>((entityID, msgReplication) =>
+        {
+            //get history of current entity in loop
+            ClientHistory entityClientHistory = ComponentsManager.Instance.GetComponent<ClientHistory>(msgReplication.entityId);
+
+            //update entity history to only contain states with time equal or greater than timestamp in the server replication message
+            while (msgReplication.timeCreated - entityClientHistory.timeCreated[0] > Time.deltaTime)
+            {
+                entityClientHistory.timeCreated.RemoveAt(0);
+                entityClientHistory.shapeComponents.RemoveAt(0);
+
+            }
+        });
+
+        //this loop is for verifying if reconcialiation is needed for the entities
+        bool reconciliationBool = false;
+        if (ECSManager.Instance.Config.enableDeadReckoning | ECSManager.Instance.Config.enableInputPrediction)
+        {
+            ComponentsManager.Instance.ForEach<ReplicationMessage>((entityID, msgReplication) =>
+            {
+                //get history of current entity in loop
+                ClientHistory entityClientHistory = ComponentsManager.Instance.GetComponent<ClientHistory>(msgReplication.entityId);
+
+                //get entity client state at time t
+                ShapeComponent pastShapeComponent = entityClientHistory.shapeComponents[0];
+
+                int threshold = 1;
+                if ((pastShapeComponent.pos - msgReplication.pos).sqrMagnitude > threshold)//if the position difference is larger than a threshold, apply reconciliation
+                {
+                    reconciliationBool = true;
+                }
+            });
+        }
+        //the loops in this section are for applying simulations
+        if (reconciliationBool)
+        {
+            ClientHistory entityClientHistory;
+            //overwrite history at time t with the server state
+            ComponentsManager.Instance.ForEach<ReplicationMessage>((entityID, msgReplication) =>
+            {
+                entityClientHistory = ComponentsManager.Instance.GetComponent<ClientHistory>(msgReplication.entityId);
+                ShapeComponent component = ComponentsManager.Instance.GetComponent<ShapeComponent>(msgReplication.entityId);
+                component.pos = msgReplication.pos;
+                component.speed = msgReplication.speed;
+                component.size = msgReplication.size;
+                entityClientHistory.shapeComponents[0] = component;
+
+            });
+
+            //simulate and overwrite histories from time t to current time
+            for (int i = 1; i < ComponentsManager.Instance.GetComponent<ClientHistory>(0).shapeComponents.Count; i++)
             {
 
+                //simulate wall collisions
+                //ComponentsManager.Instance.ForEach<ClientHistory>((entityID, history) =>
+                //{
+                  //  WallCollisionDetectionSystem.WallCollisionDetection()
+                //});
+                //    //simulate entity collisions
+                //    ComponentsManager.Instance.ForEach<ClientHistory>((entityID, history) =>
+                //    {
 
-                if ((uint)entityID == (uint)ECSManager.Instance.NetworkManager.LocalClientId)//if is client entity
+                //    });
+                //    //simulate bounce
+                //    ComponentsManager.Instance.ForEach<ClientHistory>((entityID, history) =>
+                //    {
+
+                //    });
+                //    //simulate position updates and overwrite history
+                //    ComponentsManager.Instance.ForEach<ClientHistory>((entityID, history) =>
+                //    {
+
+                //    });
+            }
+            
+        }
+        
+
+        //this loop is for applying the corrected/simulated state to the entities
+        ComponentsManager.Instance.ForEach<ReplicationMessage>((entityID, msgReplication) =>
+        {
+            //if is client entity
+            if ((uint)entityID == (uint)ECSManager.Instance.NetworkManager.LocalClientId)
+            {
+                if (ECSManager.Instance.Config.enableInputPrediction)
                 {
-                    if (ECSManager.Instance.Config.enableInputPrediction)
-                    {
-                        //Debug.Log("InputPred enabled");
-                    }
-                    else
-                    {
-                        component.pos = msgReplication.pos;
-                        component.speed = msgReplication.speed;
-                        component.size = msgReplication.size;
-                        ComponentsManager.Instance.SetComponent<ShapeComponent>(msgReplication.entityId, component);
-                        //Debug.Log("InputPred not enabled");
-                    }
+                    //Debug.Log("InputPred enabled");
+
                 }
-                else//if is any other entity than client
+                else
                 {
-                    if (ECSManager.Instance.Config.enableDeadReckoning)
-                    {
-                        //Debug.Log("DeadReckoning enabled");
-                        int timeCreated;
-                        ShapeComponent shapeComponent;
-                        List<ClientHistory> playerHistories = new List<ClientHistory>();
+                    //Debug.Log("InputPred not enabled");
+                    ShapeComponent component = ComponentsManager.Instance.GetComponent<ShapeComponent>(msgReplication.entityId);
+                    component.pos = msgReplication.pos;
+                    component.speed = msgReplication.speed;
+                    component.size = msgReplication.size;
+                    ComponentsManager.Instance.SetComponent<ShapeComponent>(msgReplication.entityId, component);
+                }
+            }
+            else//if is not the client
+            {
+                if (ECSManager.Instance.Config.enableDeadReckoning)
+                {
+                    //Debug.Log("DeadReckoning enabled");
 
-                        ////get history of all players
-                        ComponentsManager.Instance.ForEach<ClientHistory, PlayerComponent>((playerEntityID, playerClientHistory, playerComponent) =>
-                        {
-                            //update player history to only contain states with time equal or greater than timestamp in the server replication message
-                            while(msgReplication.timeCreated - playerClientHistory.timeCreated[0] > Time.deltaTime)
-                            {
-                                playerClientHistory.timeCreated.RemoveAt(0);
-                                playerClientHistory.shapeComponents.RemoveAt(0);
-
-                            }
-                            //get history of this player
-                            playerHistories.Add(playerClientHistory);
-                        });
-
-                        //get history of current entity in loop
-                        ClientHistory entityClientHistory = ComponentsManager.Instance.GetComponent<ClientHistory>(msgReplication.entityId);
-                        //update entity history to only contain states with time equal or greater than timestamp in the server replication message
-                        while (msgReplication.timeCreated - entityClientHistory.timeCreated[0] > Time.deltaTime)
-                        {
-                            entityClientHistory.timeCreated.RemoveAt(0);
-                            entityClientHistory.shapeComponents.RemoveAt(0);
-
-                        }
+                    ShapeComponent component = ComponentsManager.Instance.GetComponent<ClientHistory>(msgReplication.entityId).shapeComponents[0];
+                    ComponentsManager.Instance.SetComponent<ShapeComponent>(msgReplication.entityId, component);
 
 
-                        //get entity client state at time t
-                        timeCreated = entityClientHistory.timeCreated[0];
-                        shapeComponent = entityClientHistory.shapeComponents[0];
-
-                        //    //get player client states at time t
-                        //    List<ShapeComponent> playerCurrentShapeComponents = new List<ShapeComponent>();
-                        //    List<uint> playerIDs = new List<uint>();
-                        //    if (msgReplication.timeCreated - timeCreated < Time.deltaTime & msgReplication.timeCreated - timeCreated > 0)//found the corresponding frame
-                        //    {
-                        //        //compare entity position from client and server sides
-                        //        if ((shapeComponent.pos - msgReplication.pos).sqrMagnitude > 0.01)//if the position difference is larger than a threshold, apply reconciliation
-                        //        {
-                        //            //apply server state from time t
-                        //            shapeComponent = ComponentsManager.Instance.GetComponent<ShapeComponent>(msgReplication.entityId);
-                        //            shapeComponent.pos = msgReplication.pos;
-                        //            shapeComponent.speed = msgReplication.speed;
-                        //            //simulate through history to where that position and speed would be now
-                        //            for (int i = 0; i < history.timeCreated.Count; i++)
-                        //            {
-                        //                //entity values at time t+dt before reconcialiation
-                        //                timeCreated = history.timeCreated.Dequeue();
-                        //                history.shapeComponents.Dequeue();//throw away old history values
-                        //                shapeComponent = SimulateUpdates(msgReplication.entityId, shapeComponent, playerHistories);
-                        //                history.timeCreated.Enqueue(timeCreated);
-                        //                history.shapeComponents.Enqueue(shapeComponent);
-                        //            }
-                        //            //set dead reckoning of final frame as current client state
-                        //            ComponentsManager.Instance.SetComponent<ShapeComponent>(msgReplication.entityId, shapeComponent);
-                        //        }
-                        //        break;//exit while loop
-
-                        //    }
-                        //    else
-                        //    {
-                        //        foreach (var playerHistory in playerHistories)
-                        //        {
-                        //            playerHistory.shapeComponents.Dequeue();//to make sure the player state to be dequeued next corresponds to the next frame's
-                        //        }
-                        //    }
-                        //}
-
-                        foreach (var playerClientHistory in playerHistories)
-                        {
-                        component.pos = playerClientHistory.shapeComponents[0].pos;
-                        component.speed = new Vector2(0,0);//entityClientHistory.shapeComponents[0].speed;
-                        component.size = msgReplication.size;
-                        }
-                        
-                        ComponentsManager.Instance.SetComponent<ShapeComponent>(msgReplication.entityId, component);
-                    }
-                    else//without dead-reckoning activated, simply replicate non-player entities 
-                    {
-                        component.pos = msgReplication.pos;
-                        component.speed = msgReplication.speed;
-                        component.size = msgReplication.size;
-                        ComponentsManager.Instance.SetComponent<ShapeComponent>(msgReplication.entityId, component);
-                        //Debug.Log("DeadReackoning not enabled");
-                    }
+                }
+                else//without dead-reckoning activated, simply replicate non-player entities 
+                {
+                    //Debug.Log("DeadReackoning not enabled");
+                    ShapeComponent component = ComponentsManager.Instance.GetComponent<ShapeComponent>(msgReplication.entityId);
+                    component.pos = msgReplication.pos;
+                    component.speed = msgReplication.speed;
+                    component.size = msgReplication.size;
+                    ComponentsManager.Instance.SetComponent<ShapeComponent>(msgReplication.entityId, component);
                 }
             }
         });
+
     }
     private static ShapeComponent SimulateUpdates(uint entityID, ShapeComponent shapeComponent, List<ClientHistory> playerHistories)
     {
